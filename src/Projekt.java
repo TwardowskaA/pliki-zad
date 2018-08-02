276f16a3b07efb6267616cfc9c3475d4039b985b
Agata <twardowska123@gmail.com>
2018-08-02 19:26:58 +0200

    zadanie prezentacja

--- src/Projekt.java
+++ src/Projekt.java
@@ -1,22 +1,31 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Projekt {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("plik.txt");
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+//            File file = new File("plik.txt");
+//            try {
+//                file.createNewFile();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//
+//            try (Scanner scan = new Scanner(file)) {
+//                while (scan.hasNextLine()) {
+//                    System.out.println(scan.nextLine());
+//                }
+//            }
 
-        try (Scanner scan = new Scanner(file)) {
-            while (scan.hasNextLine()) {
-                System.out.println(scan.nextLine());
-            }
-        }
+        FileWriter fw = new FileWriter("abc.txt", true);
+        BufferedWriter bfw = new BufferedWriter(fw);
+        bfw.write("Ala");
+        bfw.newLine();
+        bfw.write("ma");
+        bfw.newLine();
+        bfw.write("kota");
+        bfw.close(); // bfw.flush
     }
+
 }
